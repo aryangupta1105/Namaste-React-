b357c0e15d8a00e2bef07faf1fef35911129fc32
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-04-30 23:29:20 +0530

    Creating food ordering app

---
+++ Day 4 Talk is cheap show me the code/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*React Components:
+-Header
+    -Logo 
+    -NavItem
+    -Cart Button
+-Body
+    -Search Bar
+    -Restaurant Container
+        -Restaurant Cards
+-Footer
+    -Copyright
+    -HyperLinks
+    -Address
+    -Contact
+ */
