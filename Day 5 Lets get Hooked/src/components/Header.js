4c467b8b68d790d1e6df7891a4147e870239b40e
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-05-02 22:16:44 +0530

    creating new history

---
+++ Day 5 Lets get Hooked/src/components/Header.js
@@ -0,0 +1,34 @@
+import { LOGO_URL } from "../utils/Constants";
+import { useState } from "react";
+
+const Header = () => {
+  const btnName = "Log In";
+  const [buttonName , setBtnName] = useState(btnName);
+
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <img
+            src={LOGO_URL}
+            alt="App Logo"
+            className="logo"
+          />
+        </div>
+        <div className="nav-items">
+          <ul className="item-container">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>🛒Cart</li>
+            <button className="login-btn" onClick={() =>{
+              // We call this setBtnName to re-render the UI
+              buttonName === "Log In"?setBtnName("Log Out") : setBtnName("Log In");
+            }}>{buttonName}</button>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+
+export default Header;
